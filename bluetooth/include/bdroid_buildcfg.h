46af79d1e2df590d23ca61dee884bd4f4d1d7f4e
bheatleyyy <sugakesshaa@gmail.com>
2022-10-03 12:20:31 +0000

    sm8350-common: switch to aosp bt stack
    This partially reverts commit 14509d9e2f66819b60dcf40dfba5c49949a61b40.
    
    * Also checkout audio policy from https://github.com/bheatleyyy/device_xiaomi_lisa/blob/thirteen/configs/audio/audio_policy_configuration.xml
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -27,7 +27,8 @@
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
+#define AVDT_NUM_SEPS 12
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
